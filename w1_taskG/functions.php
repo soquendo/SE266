46b73896e09b1a626ba28852af7ab417bb46bf9f
Sebastian Oquendo <soquendo@email.neit.edu>
2022-11-09 03:36:06 -0500

    dfsdfsdf

---
+++ w1_taskG/functions.php
@@ -0,0 +1,11 @@
+<?php
+
+function fizzBuzz($data){
+
+echo '<pre>';
+
+die(var_dump($data));
+
+echo '</pre>';
+
+}
---
+++ w1_taskG/index.php
@@ -0,0 +1,11 @@
+<?php
+
+require 'functions.php';
+
+for ($i = 0; $i <=100; $i++){
+    if (($i + 1) % 2 == 0))
+        echo "fizz";
+}
+
+
+require 'index.view.php';
