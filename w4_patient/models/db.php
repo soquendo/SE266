10ead1e5ba874487f129f7f03a0bb8d3bdbcda7a
Sebastian Oquendo <soquendo@email.neit.edu>
2022-12-16 11:52:08 -0500

    qwert

--- w4_patient/models/db.php
+++ w4_patient/models/db.php
@@ -1,14 +1,11 @@
 <?php
 
 $ini = parse_ini_file(__DIR__ . './dbconfig.ini');
-var_dump($ini);
-exit;
 
-$db = new PDO(
-                "mysql:host=" . $ini['servername'] .
-                ";port=" . $ini['port'] .
-                ";dbname=" . $ini['dbname'],
-                $ini['username'],
-                $ini['password']);
+$db = new PDO("mysql:host=" . $ini['servername'] . 
+              ";port=" . $ini['port'] . 
+              ";dbname=" . $ini['dbname'], 
+              $ini['username'], 
+              $ini['password']);
 
 $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
